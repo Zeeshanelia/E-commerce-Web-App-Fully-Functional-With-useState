c99230c8c1a0456c2e565340f53bca19109b12d6
Zeeshanelia <Zeeshanelia@gmail.com>
2026-01-12 23:24:02 +0500

    WishList Ui

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
         <Banner />
         <Products searchedProduct={searchedProduct} />
         <Cart />
-        {/* <WishList /> */}
+        <WishList />
     </>)
 }
 
